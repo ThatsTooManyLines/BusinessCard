aec77e7a9c6eb4701e6b1ee82943655bb90189c4
ThatsTooManyLines <82187267+ThatsTooManyLines@users.noreply.github.com>
2021-04-13 19:27:13 -0500

    Add files via upload

---
+++ BusinessCrad_Intentional.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Big Laugho" << std::endl;
+    std::cout << "8008 Leyo Dr, San Antonio TX 78213" << std::endl;
+    std::cout << "+18005551337" << std::endl;
+    std::cout << "http://www.LaughAllNow.com" << std::endl;
+    return 0;
+}
